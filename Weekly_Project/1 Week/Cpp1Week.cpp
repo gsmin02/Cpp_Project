7fa202a34fd7042f10a1c43318561f4ed019eb87
gsmin02 <86878502+gsmin02@users.noreply.github.com>
2021-09-02 22:07:37 +0900

    Update Cpp1Week.cpp

--- Weekly_Project/1 Week/Cpp1Week.cpp
+++ Weekly_Project/1 Week/Cpp1Week.cpp
@@ -1,38 +1,33 @@
-﻿// C vs C++
-// C
-/*
+﻿// C 기본 출력
 #include <stdio.h>
 int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
-*/
 
-// C++
-/*
+
+// C++ 기본 출력
 #include <iostream>
 using namespace std;
 int main() {
 	cout <<"Hello World"<<endl;
 	return 0;
 }
-*/
+
 
 // C++ 출력 연습
-/*
 #include <iostream>
 
 int main() {
-	std::cout << "1.구승민\n";
-	std::cout << "2.구승민\n";
-	std::cout << "3.구승민\n";
-	std::cout << "4.구승민\n";
-	std::cout << "5.구승민\n";
-	std::cout << "6.구승민\n";
-	std::cout << "7.구승민\n";
-	std::cout << "8.구승민\n";
-	std::cout << "9.구승민\n";
-	std::cout << "10.구승민\n";
+	std::cout << "1.Name\n";
+	std::cout << "2.Name\n";
+	std::cout << "3.Name\n";
+	std::cout << "4.Name\n";
+	std::cout << "5.Name\n";
+	std::cout << "6.Name\n";
+	std::cout << "7.Name\n";
+	std::cout << "8.Name\n";
+	std::cout << "9.Name\n";
+	std::cout << "10.Name\n";
 	return 0;
 }
-*/
